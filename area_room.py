e9a8503ccdd07a0d61c10b1f02c3bf0182a78325
MaxMaxM <kohotokk@mail.ru>
2018-04-17 21:21:43 +0300

    add area_room

---
+++ area_room.py
@@ -0,0 +1,28 @@
+class Wind_Door():
+    def __init__(self, w, h):
+         self.square = w * h
+
+class Room():
+    def __init__(self, x, y, z):
+        self.square = 2 * z * (x + y)
+
+    def win_door(self, d,e, f,g, m=1, n=1):
+        self.window = Wind_Door(d, e)
+        self.door = Wind_Door(f, g)
+        self.numb_w = m
+        self.numb_n = n
+
+    def wallpapers(self):
+        self.wallpapers = (self.square -
+                self.window.square * self.numb_w -
+                self.door.square * self.numb_n)
+
+    def printer(self):
+        print('The area of the wall is equal: {0} m2'.format(self.square))
+        print('The area of the wallpapers: {0} m2'.format(str(self.wallpapers)))
+
+room = Room(5, 4, 2)
+room.win_door(1.5, 1.5, 2,1, 2)
+room.wallpapers()
+room.printer()
+
